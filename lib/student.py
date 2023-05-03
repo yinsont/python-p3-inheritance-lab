68165e5236e0c604a1dd590e02b6248ac52e4890
Yinson Tso <yinson.tso@gmail.com>
2023-05-03 16:28:50 -0400

    yes

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,10 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.knowledge = []
+
+    def learn(self, str):
+        self.knowledge.append(str)
